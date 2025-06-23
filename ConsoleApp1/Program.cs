e35b878b039dae3034d03fd0ac6677fdbe9740e2
dina Muhammed farghly <dinamuhammed2001@gmail.com>
2025-06-23 23:52:57 +0300

    Q1solve

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace AssignmentSolutions
             Console.Write("Enter a number: ");
             int number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("You entered: " + number);
-            Console.WriteLine();
+            
             #endregion
 
             #region 2
